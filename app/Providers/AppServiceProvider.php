acc775472d5b12abc4ea81bc5c5d3df35c4ecbc3
Hani MEBTOUCHE <mebtouche.hani@epal.dz>
2026-05-14 10:35:33 +0100

    fix case controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Database\Eloquent\Model;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,16 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Model::$dateFormat = 'Y-m-d H:i:s';
+
 
         Paginator::useBootstrapFive();
-        
+
         \Illuminate\Support\Collection::macro('recursive', function () {
             return $this->map(function ($value) {
                 if (is_array($value) || is_object($value)) {
                     return collect($value)->recursive();
                 }
-        
+
                 return $value;
             });
         });
